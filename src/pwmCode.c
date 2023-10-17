bc3e4034dad40fde6d55c04d10c093a5e1ca0a05
mclark <mclark95@gmail.com>
2023-10-17 12:27:08 -0700

    Testing Servo with ADC

--- src/pwmCode.c
+++ src/pwmCode.c
@@ -16,19 +16,16 @@ void PWMConfigure(void) {
     SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;                                //Enable GPIO Port E
     while(!(SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R4)) {}                         //Wait for peripheral to be ready
 
-    GPIO_PORTF_AFSEL_R |= (1 << 5);                                         //Set Alternate Function for PE5
-    GPIO_PORTF_PCTL_R |= GPIO_PCTL_PE5_M0PWM5;                              //Set Port Control to PE5 PWM value
-    GPIO_PORTF_DEN_R |= (1 << 5);                                           //Set Digital Enable for PE5
+    GPIO_PORTE_AFSEL_R |= (1 << 5);                                         //Set Alternate Function for PE5
+    GPIO_PORTE_PCTL_R |= GPIO_PCTL_PE5_M0PWM5;                              //Set Port Control to PE5 PWM value
+    GPIO_PORTE_DEN_R |= (1 << 5);                                           //Set Digital Enable for PE5
 
     PWM0_2_CTL_R = 0x0;                                                     //Disable PWM0 Gen2
     PWM0_2_GENB_R = PWM_2_GENB_ACTCMPBD_ONE | PWM_2_GENB_ACTLOAD_ZERO | PWM_2_GENB_ACTZERO_ONE;
                                                                             //Set PWM1 Gen2-B comparator, load, and zero
 
     PWM0_2_LOAD_R = CYCLES_PER_MS * 20;                                     //Set PWM Gen2 Period to 1ms
-
-    /* Calculate Initial Value for Duty Cycle (Angle = 0) */
-    duty_cycle = ((set_angle * (MAX_PULSE - MIN_PULSE)/DEG_OF_ROTATION) + MIN_PULSE) / 20;
-    PWM0_2_CMPB_R = CYCLES_PER_MS_DIV_100 * (duty_cycle);                   //Set initial duty cycle
+    PWM0_2_CMPB_R = 0;                                                      //Set initial duty cycle
 
     PWMEnable();                                                            //Enable PWM
 
